33779bd9174b3835202f3aa443a50d7be8a6a4e6
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2021-02-22 17:58:51 +0000

    Do various evil things to make DTrace stack() work much better on aarch64.
    This includes:
    
    - Detect when we're within exception handlers and decode the trap frame
      to more properly show trap-related events.
    - Deduplicate the resulting PCs in the event we're generating the stack
      trace from within a function without a stack frame.
    - Add various bits of debug tooling to be deleted at the earliest
      opportunity.

--- sys/cddl/dev/dtrace/aarch64/dtrace_isa.c
+++ sys/cddl/dev/dtrace/aarch64/dtrace_isa.c
@@ -31,6 +31,7 @@
 #include <sys/systm.h>
 #include <sys/kernel.h>
 #include <sys/stack.h>
+#include <sys/sysctl.h>
 #include <sys/pcpu.h>
 
 #include <machine/frame.h>
@@ -64,29 +65,162 @@ uint16_t dtrace_fuword16_nocheck(void *);
 uint32_t dtrace_fuword32_nocheck(void *);
 uint64_t dtrace_fuword64_nocheck(void *);
 
+extern void *__handle_el1h_irq_start;
+extern void *__handle_el1h_irq_end;
+extern void *__handle_el1h_sync_start;
+extern void *__handle_el1h_sync_end;
+
+SYSCTL_DECL(_kern_dtrace);
+
+/*
+ * Force aframes to zero regardless of the provider.
+ */
+static int kern_dtrace_pcstack_aframes_zero;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, pcstack_aframes_zero, CTLFLAG_RW,
+    &kern_dtrace_pcstack_aframes_zero, 0,
+    "Force DTrace stack-trace aframes to zero");
+
+/*
+ * Only record frames at or after the passed PC value.
+ */
+static int kern_dtrace_stack_start_at_intrpc = 0;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, stack_start_at_intrpc, CTLFLAG_RW,
+    &kern_dtrace_stack_start_at_intrpc, 0,
+    "Only record stack frames once we've hit the exception PC");
+
+/*
+ * Enable trap-frame handling.
+ */
+static int kern_dtrace_stack_trapframes = 1;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, stack_trapframes, CTLFLAG_RW,
+    &kern_dtrace_stack_trapframes, 0,
+    "Enable parsing of trap frames");
+
+/*
+ * Enable handling of aframes.
+ */
+static int kern_dtrace_aframes = 1;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, aframes, CTLFLAG_RW, &kern_dtrace_aframes,
+    0, "Enable processing of aframes requested by the provider");
+
+/*
+ * Enable artifical frames at the start of the trace.
+ */
+static int kern_dtrace_pcstack_debug = 0;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, pcstack_debug, CTLFLAG_RW,
+    &kern_dtrace_pcstack_debug, 0,
+    "Inject psecial frames with debug data");
+
+/*
+ * Allow dtrace_caller to replace intrpc when defined.
+ */
+static int kern_dtrace_pcstack_dtracecaller = 1;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, pcstack_dtracecaller, CTLFLAG_RW,
+    &kern_dtrace_pcstack_dtracecaller, 0,
+    "Allow cpu->pc_dtrace_caller to replace intrprc for FDT probes");
+
+static int kern_dtrace_pcstack_pcfound;
+SYSCTL_INT(_kern_dtrace, OID_AUTO, pcstack_pcfound, CTLFLAG_RD,
+    &kern_dtrace_pcstack_pcfound, 0,
+    "Count of the number of times intrpc matched, enabling tracing");
+
+static void
+dtrace_pushstack(pc_t *pcstack, int pcstack_limit, int *aframesp, int *depthp,
+    intptr_t pc, int *pc_foundp, intptr_t pc_target)
+{
+
+	if (kern_dtrace_stack_start_at_intrpc && pc_foundp != NULL) {
+		if (!(*pc_foundp)) {
+			if (pc != pc_target)
+				return;
+			*pc_foundp = 1;
+			kern_dtrace_pcstack_pcfound++;
+		}
+	}
+	if (*depthp == pcstack_limit)
+		return;
+	if (kern_dtrace_aframes && aframesp != NULL) {
+		if ((*aframesp) > 0) {
+			(*aframesp)--;
+			return;
+		}
+	}
+	pcstack[*depthp] = pc;
+	(*depthp)++;
+}
+
+static void
+dtrace_pushstack_nopcfound(pc_t *pcstack, int pcstack_limit, int *aframesp,
+    int *depthp, intptr_t pc)
+{
+
+	dtrace_pushstack(pcstack, pcstack_limit, aframesp, depthp, pc, NULL,
+	    0);
+}
+
+static int
+dtrace_el1_handler_pc(register_t pc)
+{
+
+	return ((pc >= (uintptr_t)&__handle_el1h_irq_start &&
+	     pc < (uintptr_t)&__handle_el1h_irq_end) ||
+	    (pc >= (uintptr_t)&__handle_el1h_sync_start &&
+	     pc < (uintptr_t)&__handle_el1h_sync_end));
+}
+
 void
 dtrace_getpcstack(pc_t *pcstack, int pcstack_limit, int aframes,
     uint32_t *intrpc)
 {
-	struct unwind_state state;
+	struct unwind_state state, nextframe_state;
+	struct trapframe *tf;
 	int scp_offset;
-	register_t sp;
+	register_t sp, exception_lr, non_exception_lr;
+	void *pc_prior;
+	int pc_found;
 	int depth;
 
+	/*
+	 * When running with FBT, intrpc is not set to a value that appears in
+	 * the stack, so instead search for cpu_dtrace_caller.  Only FBT sets
+	 * up cpu_dtrace_caller, and clears it after the probe completes.
+	 */
+	if (kern_dtrace_pcstack_dtracecaller &&
+	    ((pc_t)solaris_cpu[curcpu].cpu_dtrace_caller != 0))
+		intrpc = (void *)(pc_t)solaris_cpu[curcpu].cpu_dtrace_caller;
+
+	/*
+	 * Can't search for the intrpc if we don't process trapframes.
+	 */
+	if (!kern_dtrace_stack_trapframes)
+		kern_dtrace_stack_start_at_intrpc = 0;
+
+	/*
+	 * If we are starting the trace with a specific intrpc, we can ignore
+	 * aframes.
+	 */
+	if (kern_dtrace_stack_start_at_intrpc ||
+	    kern_dtrace_pcstack_aframes_zero)
+		aframes = 0;
 	depth = 0;
-
-	if (intrpc != 0) {
-		pcstack[depth++] = (pc_t) intrpc;
-	}
-
-	aframes++;
+	pc_found = 0;
+	pc_prior = NULL;
 
 	__asm __volatile("mov %0, sp" : "=&r" (sp));
-
 	state.fp = (uintptr_t)__builtin_frame_address(0);
 	state.sp = sp;
 	state.pc = (uintptr_t)dtrace_getpcstack;
 
+	if (kern_dtrace_pcstack_debug) {
+		dtrace_pushstack_nopcfound(pcstack, pcstack_limit, NULL,
+		    &depth, 0x2b2b);
+		dtrace_pushstack_nopcfound(pcstack, pcstack_limit, NULL,
+		    &depth, (uintptr_t)intrpc);
+		dtrace_pushstack_nopcfound(pcstack, pcstack_limit, NULL,
+		    &depth, aframes);
+		dtrace_pushstack_nopcfound(pcstack, pcstack_limit, NULL,
+		    &depth, 0x2b2b);
+	}
 	while (depth < pcstack_limit) {
 		if (!unwind_frame(curthread, &state))
 			break;
@@ -94,18 +228,87 @@ dtrace_getpcstack(pc_t *pcstack, int pcstack_limit, int aframes,
 			break;
 
 		/*
-		 * NB: Unlike some other architectures, we don't need to
-		 * explicitly insert cpu_dtrace_caller as it appears in the
-		 * normal kernel stack trace rather than a special trap frame.
+		 * If the stack frame belongs to either the _sync or _irq EL1
+		 * exception handler, then we expect an adjacent trapframe
+		 * from which we can also extract state.  Using that frame's
+		 * ELR (and perhaps LR) will give a more accurate stack trace
+		 * than using the synthetic frame pushed by the exception
+		 * handler.
 		 */
-		if (aframes > 0) {
-			aframes--;
-		} else {
-			pcstack[depth++] = state.pc;
-		}
+		if (kern_dtrace_stack_trapframes &&
+		    dtrace_el1_handler_pc(state.pc)) {
+			/*
+			 * Print the exception-handler PC reliably first.
+			 */
+			dtrace_pushstack(pcstack, pcstack_limit, &aframes,
+			    &depth, state.pc, &pc_found, (intptr_t)intrpc);
 
+			/*
+			 * Now find and inspect the stack frame.
+			 */
+			tf = (struct trapframe *)(uintptr_t)state.fp - 1;
+
+			/*
+			 * XXX: We need to handle the case where there's no
+			 * stack frame for the preempted function, and so we
+			 * skip a frame.  But how to distinguish from the
+			 * other cases?  For now, insert LR twice rather than
+			 * no times, but need to do better.
+			 */
+			if (kern_dtrace_pcstack_debug)
+				dtrace_pushstack_nopcfound(pcstack,
+				    pcstack_limit, &aframes, &depth, 0xeeee);
+			dtrace_pushstack(pcstack, pcstack_limit, &aframes,
+			    &depth, tf->tf_elr, &pc_found, (intptr_t)intrpc);
+
+			/*
+			 * NB: As with unwind_state(), trim 4 from LR to give
+			 * what is presumed to be the caller site, not the
+			 * LR target.
+			 */
+			pc_prior = (void *)(tf->tf_lr - 4);
+			dtrace_pushstack(pcstack, pcstack_limit, &aframes,
+			    &depth, tf->tf_lr - 4, &pc_found, (intptr_t)intrpc);
+			if (kern_dtrace_pcstack_debug)
+				dtrace_pushstack_nopcfound(pcstack,
+				    pcstack_limit, &aframes, &depth, 0xeeee);
+
+			/*
+			 * Use exception frame to find the next stack frame
+			 * up.
+			 *
+			 * XXXRW: What if it has no frame .. should we be
+			 * using LR instead ..?
+			 */
+			state.fp = tf->tf_x[29];
+			state.pc = tf->tf_elr;
+			if (!INKERNEL(state.pc))
+				break;
+			if (!INKERNEL(state.fp))
+				break;
+		} else {
+			/*
+			 * Print a regular frame.
+			 *
+			 * If the PC is identical to the one we just printed
+			 * on the way out of an exception return, suppress the
+			 * duplicate.  Most likely we were in a function
+			 * that didn't have its own stack frame (e.g.,
+			 * memcpy).  In the kernel, it's unlikely to be a
+			 * recursive call.
+			 */
+			if (pc_prior == NULL ||
+			    (pc_prior != NULL && pc_prior != (void *)state.pc))
+				dtrace_pushstack(pcstack, pcstack_limit,
+				    &aframes, &depth, state.pc, &pc_found,
+				    (intptr_t)intrpc);
+			pc_prior = NULL;
+		}
 	}
 
+	if (kern_dtrace_pcstack_debug)
+		dtrace_pushstack_nopcfound(pcstack, pcstack_limit, &aframes,
+		    &depth, 0xe0e0);
 	for (; depth < pcstack_limit; depth++) {
 		pcstack[depth] = 0;
 	}
